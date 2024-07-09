48f86ee27db1ee9d0a906ed8f6ca4d16de9e7087
Gerson Contreras <78560393+gerssoonn@users.noreply.github.com>
2024-07-09 11:19:44 -0400

    agregar desafio intro a js

---
+++ javascript/Material de Apoyo - Base desafío calculando el total/index.js
@@ -0,0 +1,21 @@
+const precioSpan = document.querySelector(".precio-inicial");
+const cantidadCompras = document.getElementById("cantidadCompras");
+const valorTotal = document.getElementById("valor-total");
+
+let cantidad = 0;
+const precio = 400000;
+
+precioSpan.innerHTML = precio;
+cantidadCompras.innerHTML = cantidad;
+
+const operacion = () => cantidad * precio;
+
+function sumar() {
+  cantidadCompras.innerText = ++cantidad;
+  valorTotal.innerText = operacion();
+}
+
+function restar() {
+  cantidadCompras.innerText = --cantidad;
+  valorTotal.innerText = operacion();
+}
